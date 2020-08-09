bb0bd4cd58ec3f86e75d8f9ff9a90a6af46dd07e
Christopher Akintade <chrisfemide@gmail.com>
2020-08-09 18:55:22 +0100

    Cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-  app.use(function(req, res, next) {
+ /**  app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept, ');
@@ -27,9 +27,9 @@ app.use(bodyParser.json());
     } else {
        next();
     }
-  }); 
+  }); */
 
-//app.use(cors());
+app.use(cors());
 
 app.use((req, res, next) => {
     if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
